bbd6cc87fdea271efb574196fb8f96c907baaace
Rafael Queiroz Vilaça <112505616+rafaqueiroz10@users.noreply.github.com>
2025-01-15 20:53:14 -0300

    Caso de uso 004, 005 e 009: começo do js para exibir as requisições na página

--- Project/PrevenTech/src/main/webapp/js/requisicoes.js
+++ Project/PrevenTech/src/main/webapp/js/requisicoes.js
@@ -1,4 +1,67 @@
 function exibirRequisicoes(data) {
+    function exibirStatus() {
+        
+    }
+    
+    function exibirTecnico() {
+        
+    }
+    
+    function exibirDataHorario() {
+        
+    }
+    
+    function exibirCategoria() {
+        
+    }
+    
+    function exibirEquipamentos() {
+        
+    }
+    
+    function exibirDescricao() {
+        
+    }
+    
+    for(let requisicao of data) {
+        let novaRequisicaoEl = document.createElement('div');
+        let viewBasicoEl = document.createElement('div');
+        let informacoesBasicasEl = document.createElement('div');
+        
+        let titleRequisicaoEl = document.createElement('h4');
+        let spanNumeroRequisicaoEl = document.createElement('span');
+        let spanNumeroRequisicoesEl = document.createElement('span');
+        
+        let informacoesExpandirEl = document.createElement('div');
+        let setinhaExpandirRetrairEl = document.createElement('div');
+        let linhaRequisicaoEl = document.createElement('div');
+        
+        let statusEl = document.createElement('div');
+        let corStatusEl = document.createElement('span');
+        let txtStatusEl = document.createElement('span');
+        
+        let tecnicoEl = document.createElement('div');
+        let spanTxtTecnicoEl = document.createElement('span');
+        let spanNomeTecnicoEl = document.createElement('span');
+    
+        let dataHorarioEnvioEl = document.createElement('div');
+        let txtEnviadoEl = document.createElement('span');
+        let txtHorarioEl = document.createElement('span');
+        let txtDataEl = document.createElement('span');
+        
+        let categoriaEl = document.createElement('div');
+        let txtCategoriaEl = document.createElement('span');
+        let nomeCategoriaEl = document.createElement('span');
+        
+        let equipamentosEl = document.createElement('div');
+        let txtEquipamentosEl = document.createElement('span');
+        let numeroDasMaquinasEl = document.createElement('span');
+        
+        let descricaoEl = document.createElement('div');
+        let tituloDescricaoEl = document.createElement('h5');
+        let spanDescricaoEl = document.createElement('span');
+    }
+    
     console.log(data);
 }
 
