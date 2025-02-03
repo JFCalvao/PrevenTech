079446a3c286048532426bffa28a1ee70b46be15
Matheusss11 <matheusgabrielcampos11@gmail.com>
2025-02-03 10:41:56 -0300

    caso de uso 010

--- Project/PrevenTech/src/main/java/cefetmg/inf/preventech/servers/Tecnico.java
+++ Project/PrevenTech/src/main/java/cefetmg/inf/preventech/servers/Tecnico.java
@@ -31,6 +31,10 @@ public class Tecnico extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+         response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
         response.setContentType("application/json;charset=UTF-8");
         HttpSession session = request.getSession(false);
 
